1e6fb8e936b7685c2de620856d0932df4ad47dec
Max Kresch <mkresch.caltech@gmail.com>
2008-02-16 03:41:12 +0000

    Added read code to g.py so that it's usable "out of the box".
    git-svn-id: svn://danse.us/DynModels/BvK_forward_dynamics@32 fe934de8-9f71-4dbe-9c93-ebca799b872c

--- g.py
+++ g.py
@@ -1,8 +1,28 @@
 #!/usr/bin/python
 
 import sys
-sys.path.append("/home/mkresch/src/inelastic/idf")
-import DOS
+
+import numpy
+from struct import pack,unpack,calcsize
+intSize = calcsize('<i')
+dubSize = calcsize('<d')
+strSize = calcsize('<s')
+version=1
+
+def read(filename='DOS'):
+  """Takes filename, returns a tuple with information and DOS as a numpy."""
+  f=open(filename,'r').read()
+  i = 0
+  filetype, = unpack('<64s',f[i:i+64*strSize])          ; i += 64*strSize
+  version,  = unpack('<i',f[i:i+intSize])               ; i += intSize
+  comment,  = unpack('<1024s',f[i:i+1024*strSize])      ; i += 1024*strSize
+  N_Bins,   = unpack('<i',f[i:i+intSize])               ; i += intSize
+  dE,       = unpack('<d',f[i:i+dubSize])               ; i += dubSize
+  DOS       = unpack('<%id' % (N_Bins),f[i:])
+  DOS = numpy.array(DOS)
+  E = numpy.arange(0,N_Bins*dE,dE)
+  return (filetype.strip('\x00'),version,comment.strip('\x00')),E,DOS
+
 
 files = sys.argv[1:]
 
@@ -11,7 +31,7 @@ g = Gnuplot.Gnuplot()
 gd = Gnuplot.Data
 
 for i in files:
-  D = DOS.read(filename=i)
+  D = read(filename=i)
   g.replot(gd(D[1],D[2],with='l lw 4'))
 
 raw_input("Press <Enter> to close up shop...")

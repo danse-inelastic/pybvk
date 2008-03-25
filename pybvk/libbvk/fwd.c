4fe2fd78010fa68708394c803427001ab90662eb
Michael McKerns <mmckerns@caltech.edu>
2008-03-25 18:47:03 +0000

    a little change to allow return of intermediates
    git-svn-id: svn://danse.us/DynModels/BvK_forward_dynamics@50 fe934de8-9f71-4dbe-9c93-ebca799b872c

--- pybvk/libbvk/fwd.c
+++ pybvk/libbvk/fwd.c
@@ -12,6 +12,9 @@ int getDOS1(int withVecs,int N,double dBin);
 int getDOS2(System* system,int type,int withVecs,int N,double dBin);
 double* getDOS3(System* system,int type,int vec,int N,double dBin,
                 int* nmbins,double** pdbins);
+double* getDOS4(System* system,int type,int vec,int N,double dBin,
+                int* nmbins,double** pdbins,int* nmq,QPoint** qps,
+                EigenValue** vs,EigenVector** es);
 //------------------------------------------
 
 // main
@@ -30,17 +33,34 @@ int main(int argc,char *argv[]) {
   if(argc>5) {               // name of system? (select from list)
     strcpy(sysname,argv[5]);
   }
+  int useFiles=0;
+  if(argc>6) {
+    useFiles=atoi(argv[6]);  // write to file intermediates? (True == 1)
+  }
   // getDOS1(withVecs,N,dBin);                // fwd1
   System* system = systemRead(sysname);       // fwd2, fwd3
   // getDOS2(system,type,withVecs,N,dBin);    // fwd2
-  int nBins;
-  double* bins;
-  double* total = getDOS3(system,type,withVecs,N,dBin,&nBins,&bins);
-  totalDosWrite(nBins,dBin,total);
+  int nBins;                                  // fwd3, fwd4
+  double* bins;                               // fwd3, fwd4
+  //double* total = getDOS3(system,type,withVecs,N,dBin,&nBins,&bins); // fwd3
+  int nq;                                     // fwd4...
+  QPoint* qs=NULL;
+  EigenValue* om2s=NULL;
+  EigenVector* pols=NULL;
+  double* total = getDOS4(system,type,withVecs,N,dBin,&nBins,&bins,
+                          &nq,&qs,&om2s,&pols); // ...fwd4
+  totalDosWrite(nBins,dBin,total);            // fwd3, fwd4...
+  int nSites = system->c->sites;
   if(withVecs == 1) {
-    int nSites = system->c->sites;
     partialDosWrite(nSites,nBins,dBin,bins);
-  }
+  }                                           // ...fwd3, fwd4
+  if(useFiles == 1) {                         // fwd4...
+    qpointWrite("WeightedQ",nq,qs);
+    eigenvalueWrite("Omega2",nq,nSites,om2s);
+    if(withVecs == 1) {
+      eigenvectorWrite("Polarizations",nq,nSites,pols);
+    }
+  }                                           // ...fwd4
   printf("Ssee program? iss kap\\\"ut.\n");
   return 1;
 }
@@ -80,7 +100,7 @@ int getDOS2(System* system,int type,int vec,int N,double dBin) {
   }
   int useFiles = 0; // NOTE: allow turn on/off 'intermediate' files
   if(useFiles == 1) {
-    qpointWrite("WeigtedQ",nq,qs);
+    qpointWrite("WeightedQ",nq,qs);
     eigenvalueWrite("Omega2",nq,nSites,om2s);
     if(vec == 1) {
       eigenvectorWrite("Polarizations",nq,nSites,pols);
@@ -111,7 +131,7 @@ double* getDOS3(System* system,int type,int vec,int N,double dBin,
   // save to file
   int useFiles = 0; // NOTE: allow turn on/off 'intermediate' files
   if(useFiles == 1) {
-    qpointWrite("WeigtedQ",nq,qs);
+    qpointWrite("WeightedQ",nq,qs);
     eigenvalueWrite("Omega2",nq,nSites,om2s);
     if(vec == 1) {
       eigenvectorWrite("Polarizations",nq,nSites,pols);
@@ -119,5 +139,34 @@ double* getDOS3(System* system,int type,int vec,int N,double dBin,
   }
   *nmbins = nBins;
   *pdbins = bins;
+  return total;
+}
+
+// fwd4: system to DOS, w/o file intermediates, return DOS & intermediates
+double* getDOS4(System* system,int type,int vec,int N,double dBin,
+                int* nmbins,double** pdbins,int* nmq,QPoint** qps,
+                EigenValue** vs,EigenVector** es) {
+  initSetup();
+
+  // get qpoints
+  int nq;
+  QPoint* qs = generateQpoints(type,system,&nq,N);
+
+  // get eigenvalues & eigenvectors
+  EigenVector* pols=NULL;
+  EigenValue* om2s = generateEigenValues(vec,system,nq,qs,&pols);
+
+  // get DOSs
+  int nBins;
+  double* bins;
+  int nSites = system->c->sites;
+  double* total = generateDOS(nSites,nq,qs,om2s,pols,dBin,&nBins,&bins);
+
+  *nmbins = nBins;
+  *pdbins = bins;
+  *nmq = nq;
+  *qps = qs;
+  *vs = om2s;
+  *es = pols;
   return total;
 }

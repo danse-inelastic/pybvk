cd751fe2ea488b37fabd0d144eeba7aa9356db4e
Jiao Lin <linjiao@caltech.edu>
2010-11-23 13:30:46 +0000

    bvk/orm/BvKBond.py: minor (tips and help texts)
    git-svn-id: svn://danse.us/DynModels/BvK_forward_dynamics@151 fe934de8-9f71-4dbe-9c93-ebca799b872c

--- pybvk/bvk/orm/BvKBond.py
+++ pybvk/bvk/orm/BvKBond.py
@@ -22,19 +22,25 @@ class Inventory(InvBase):
     matter = InvBase.d.reference(name='matter', targettype=Structure, owned=0)
     uses_primitive_unitcell = InvBase.d.bool(name='uses_primitive_unitcell', default=1)
 
-    A = InvBase.d.int(name='A')
-    A.help = 'Index of atom A in the atom list in the atomic structure'
+    A = InvBase.d.int(name='A', label='Atom 1 index')
+    A.tip = 'Index of atom 1 of the bond in the atom list of the atomic structure'
+    A.help = 'A bond has two atoms. This is for the first atom in the bond. You will need to input the index of this atom in the list of all atoms in the unit cell (index starts from 0 and ends with N_atoms-1)'
     
-    B = InvBase.d.int(name='B')
-    B.help = 'Index of atom B in the atom list in the atomic structure'
+    B = InvBase.d.int(name='B', label='Atom 2 index')
+    B.tip = 'Index of atom 2 of the bond in the atom list of the atomic structure'
+    B.help = 'A bond has two atoms. This is for the second atom in the bond. You will need to input the index of this atom in the list of all atoms in the unit cell (index starts from 0 and ends with N_atoms-1)'
 
     Boffset = InvBase.d.array(
-        name='Boffset', elementtype='float',
+        name='Boffset', 
+        elementtype='float',
         default=[0,0,0], shape=3)
-    Boffset.help = 'Offset vector of atom B relative to its canonical position defined in the atomic structure'
-    
+    Boffset.label = 'Atom 2 offset'
+    Boffset.tip = 'Offset vector of atom 2 relative to its canonical position defined in the atomic structure'
+    Boffset.help = 'A bond has two atoms. This is for the second atom in the bond. You will need to input the offset of the atom 2 relative to its canonical position inside the unitcell'
+
     Boffset_is_fractional = InvBase.d.bool(name='Boffset_is_fractional', default=0)
-    Boffset_is_fractional.tip = 'When set, the offset vector of atom B is in fractional coordinates. Otherwise, it is in cartesian coordinates'
+    Boffset_is_fractional.label = 'Atom 2 offset is fractional'
+    Boffset_is_fractional.tip = 'When set, the offset vector of atom 2 is in fractional coordinates. Otherwise, it is in cartesian coordinates'
 
     force_constant_matrix = InvBase.d.array(
         name='force_constant_matrix', elementtype='float',
